b644437ac6bb1fcd90ee32787448da8edac7d4de
mrnhys <trungxk21@gmail.com>
2024-03-10 00:23:23 +0700

    first commit

---
+++ aa.js
@@ -0,0 +1,6 @@
+document.getElementById("yesButton").addEventListener("click", function() {
+    window.location.href = "/html/trang2.html"; // Thay đổi đường dẫn thành trang hoặc vị trí bạn muốn chuyển đến
+});
+document.getElementById("noButton").addEventListener("click", function() {
+    window.location.href = "/html/trang1.html"; // Thay đổi đường dẫn thành trang hoặc vị trí bạn muốn chuyển đến
+});
